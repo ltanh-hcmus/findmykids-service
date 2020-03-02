7ebd6c3658de18eb5a0efb349b687fd4a7c5f289
Lê Tuấn Anh <leanhcntt@gmail.com>
2020-03-02 10:03:32 +0700

    body validate data

--- FindMyKids.FamilyService/Models/Member.cs
+++ FindMyKids.FamilyService/Models/Member.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FindMyKids.FamilyService.Models
 {
     public class Member
     {
         public Guid ID { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [MinLength(5)]
+        [MaxLength(50)]
         public string UserName { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [MinLength(8)]
+        [MaxLength(16)]
         public string PassWord { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(150)]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")]
         public string Email { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(100)]
         public string Name { get; set; }
     }
 }
